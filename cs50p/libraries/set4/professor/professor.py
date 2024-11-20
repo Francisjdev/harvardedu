04c8728367316a3f8d8899115d1a31707ff116b5
Francis <franciscorojasdev@proton.me>
2024-11-20 19:31:33 -0300

    working on the score trackin var

--- cs50p/libraries/set4/professor/professor.py
+++ cs50p/libraries/set4/professor/professor.py
@@ -1,11 +1,11 @@
 from random import randint
-
 problems = []
+score=0
 
 def main():
     get_level()
-
-
+    solve_problems(problems,score)
+    print(score)
 def get_level():
     try:
         digits = int(input("Level: "))
@@ -26,13 +26,41 @@ def random_with_N_digits(n):
     rnum1=randint(range_start, range_end)
     rnum2=randint(range_start, range_end)
     prob=(f"{str(rnum1)}+{str(rnum2)}")
-    return prob
+    return prob 
     
 def generate_problems(digit):
     for problem in range(10):
-      problems.append(random_with_N_digits(digit))  
-
+      problems.append(random_with_N_digits(digit)) 
+    
     print(problems) 
+    return problems
+
+def solve_problems(arr,tracker):
+    for prob in arr:
+        num1,num2 = prob.split("+")
+        results=int(num1)+int(num2)
+        print(prob)
+        tracker =get_answer(results,tracker)
+    return tracker
+
+def get_answer(a,tracker):
+    x=0
+    while True:
+       
+        answer = int(input("answer: "))
+        if answer == a:
+                tracker +=1
+                break
+        else:
+                print("EEE")
+                x+=1
+                print(x)
+                if x==3:
+                    print(f"the answer is : {a}")
+                    break
+        return tracker
+    
+
 
 if __name__ == "__main__":
-    main()
+    main() 
